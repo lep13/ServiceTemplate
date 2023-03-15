084b7595b68e7923888d9c64c7d6e14339fdeb3e
Vishwanath Ucchaid <120455546+vucchaid@users.noreply.github.com>
2023-03-15 10:12:52 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"service-template/cmd/api"
+)
+
+const (
+	host = "localhost"
+	port = "8080"
+)
+
+func init() {}
+
+func main() {
+
+	err := api.ServeHTTP(fmt.Sprintf("%s:%s", host, port))
+	if err != nil {
+		log.Fatalf("[ERROR]: %s", err)
+	}
+}
