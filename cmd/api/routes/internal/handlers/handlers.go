f4b7424c9d94e4c1ab73b5bc37f91e80a98c7c9d
Vishwanath Ucchaid <120455546+vucchaid@users.noreply.github.com>
2023-03-15 10:17:59 +0530

    Create handlers.go

---
+++ cmd/api/routes/internal/handlers/handlers.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"service-template/pkg/services/healthcheck"
+)
+
+type handlerService struct{}
+
+// New Service
+func NewHandlerService() *handlerService {
+	return &handlerService{}
+}
+
+// Implementation goes here...
+func (service *handlerService) GetHealthStatus(w http.ResponseWriter, r *http.Request) {
+
+	status := healthcheck.GetHealthStatus()
+
+	w.WriteHeader(200)
+	err := json.NewEncoder(w).Encode(status)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, `{"error":"Internal server error"}`)
+		return
+	}
+
+}
